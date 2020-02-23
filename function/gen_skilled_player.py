9a8391b2435ea34e1016c08de67aab6bdc968822
Sean Morris <seanwaa@gmail.com>
2020-02-23 16:38:19 +0000

    removed outdated debug text

--- function/gen_skilled_player.py
+++ function/gen_skilled_player.py
@@ -6,12 +6,8 @@ from function.gen_skills import gen_skill_pool
 
 
 def gen_skilled_player(team_profile, skills_col):
-    # DEBUG - write some tests Sean, come on
-    print(skill_pool)
     skills = []
     starting_skills = team_profile.get('starting_skills')
-    # DEBUG
-    print(starting_skills)
 
     while len(skills) < 3:
         skill = select_skills(gen_skill_pool(team_profile, skills_col))
